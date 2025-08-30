9650a523f3ac9b410e0482ac65b4ee3c786e01c0
ranzz123 <yantisipa8@gmail.com>
2025-08-30 10:48:03 +0700

    Update token list

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8457382340:AAFmbnnTJNQNdwrCJmmnxPn_oz1jT9gysZ4"
+    "8457382340:AAFmbnnTJNQNdwrCJmmnxPn_oz1jT9gysZ4",
+    "8166526604:AAG0bNS7E37AyU8Pdc13AV5GgnGrZBYO108"
   ]
 }
